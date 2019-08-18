da3629f095fcb749a2c545242398285eb890fdf8
VeselinovStf <veselinovStf@gmail.com>
2019-08-18 07:58:46 +0300

    Fixing Profile menu link - you must authenticated to see it and use it

---
+++ Part_01/PluralSightBook.Web/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PluralSightBook.Web.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EditProfile(string a)
+        {
+            return View();
+        }
+    }
+}

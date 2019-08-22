69ba9575d5a72415b8bac6fbe7e45b1b6cdff436
VeselinovStf <veselinovStf@gmail.com>
2019-08-22 08:17:58 +0300

    Updating Friend Model - adding is deleted field

--- Part_01/PluralSightBook.Web/Data/Models/Friend.cs
+++ Part_01/PluralSightBook.Web/Data/Models/Friend.cs
@@ -7,6 +7,8 @@ namespace PluralSightBook.Web.Data.Models
         public int Id { get; set; }
         public string Email { get; set; }
 
+        public bool IsDeleted { get; set; }
+
         public string PluralSightBookIdentityUser { get; set; }
 
         public PluralSightBookIdentityUser IdentityUser { get; set; }

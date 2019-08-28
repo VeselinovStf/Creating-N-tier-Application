5104396ff8922fab7ff14fba76047b533f557430
VeselinovStf <veselinovStf@gmail.com>
2019-08-28 10:31:35 +0300

    Adding FriendController - exposing list

---
+++ Part_01/PluralSightBook.API/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PluralSightBook.Core.Identity;
+using PluralSightBook.Core.Interfaces;
+using PluralSightBook.Core.Services;
+using PluralSightBook.Infrastructure.Data;
+using PluralSightBook.Infrastructure.Repositories;
+
+namespace PluralSightBook.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureDbConnectionSettings(services);
+            ConfigureIdentitySettings(services);
+
+            services.AddTransient<IProfileService, ProfileService>();
+            services.AddTransient<IFriendService, FriendsService>();
+            services.AddTransient<IProfileRepository, EfProfileRepository>();
+            services.AddTransient<IFriendRepository, EfFriendRepository>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        private void ConfigureIdentitySettings(IServiceCollection services)
+        {
+            services.AddIdentity<PluralSightBookIdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<PluralSightBookDbContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        private void ConfigureDbConnectionSettings(IServiceCollection services)
+        {
+            services.AddDbContext<PluralSightBookDbContext>(options =>
+             options.UseSqlServer(
+               Configuration.GetConnectionString("DevelopmentConnectionString")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
